f6c6bcc0d4992689f0cd9afb5e687999cdfc877c
hafizkalam <hafizkalamabdillah@gmail.com>
2021-10-08 01:46:51 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
+    $host   ="10.0.0.194";
+    $user ="root";
+    $password ="";
+    $db ="scrud"; 
+    
+    $config =  mysqli_connect($host, $user, $password,$db);
+    if(mysqli_connect_errno())
+    {
+    echo'Koneksi database gagal:'.mysqli_connect_error();
+    }
+    else {
+    }
+    error_reporting(0);
+?>
